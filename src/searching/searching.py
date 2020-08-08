602469c49030bc4c600435f69f43556108c06805
Troy Moore <tbmoore85@gmail.com>
2020-08-07 21:56:36 -0400

    Starting

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,29 @@
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+
+
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    #if array is not none
+    #find a mid point (start + end) // 2
+    #if mid point == target return target
+    #if mid > target: 
+    #   binary_search(arr, target, start, mid)
+    #else:
+    #   binary_search(arr,target,mid,end)
+    if arr is not None:
+        middle = (start + end) // 2
+        if arr[middle] == target:
+            return middle
+        else:
+            if middle > target:
+                binary_search(arr,target,start,middle)
+            else:
+                binary_search(arr,target,middle,end)
+    return -1 
+
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +34,8 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
+
+binary_search(arr1, -8, 0, len(arr1)-1)
+
 
